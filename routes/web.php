2d5d718e5507023637b705eba891b07ec192c828
emanuelleferraz <manu.ferraz16@gmail.com>
2024-10-13 15:42:04 -0300

    criação do front-end

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [ProjectsController::class, 'index'])->name('projects.index');
 
 Route::get('/project/{project}', [ProjectsController::class, 'show'])->name('projects.show');
-
